0e03a3f982d451b90a6b2192eefed604d8885076
Van Bui <van.bt@trans-cosmos.com.vn>
2020-05-07 10:58:07 +0700

    count jewel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+constructor(props) {
+  super(props);
+  this.state ={
+    jewel:'',
+    stone:''
+  }
 }
 
-export default App;
+  onChange = (e) => {
+    var target = e.target;
+    var name = target.name;
+    var value = target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    var index = this.getSameCount(this.state.stone,this.state.jewel);
+    console.log(index);
+    
+  }
+
+  getSameCount = (stone,jewel) => {
+    let count = 0;
+    const obj = jewel.split("");
+    for(let str of stone){
+      let idx = obj.findIndex(s => s === str);
+      if(idx >= 0){
+        count++;
+        obj.splice(idx, 1);
+      }
+    }
+    return count;
+  }
+  
+
+  render() {  
+
+    return (
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Find a jewel in the stones string
+          </p>
+          
+        </header>
+        <div className="row">
+          <div className="col-4 mx-auto text-left">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label>J - jewels strings</label>
+                <input type="input" className="form-control" id="jewels"  name="jewel" onChange={ this.onChange }/>
+              </div>
+              <div className="form-group">
+                <label>S - stones strings</label>
+                <input type="input" className="form-control" id="stones" name="stone" onChange={ this.onChange }/>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
